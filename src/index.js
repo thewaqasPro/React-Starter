157eb0afc21eeaf98dfd5950e0c5dfe03b61c113
Muhammad Waqas <waqas.attary@gmail.com>
2021-01-02 14:35:08 +0500

    Start

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import isEmail from 'validator/lib/isEmail'
+import tripleMe from './tripleMe'
+
+console.log(isEmail('waqas@mail.com'));
+console.log(tripleMe(72));
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        port: 8080,
+        contentBase: path.resolve(__dirname, 'dist')
+    },
+    mode: "development"
+    // mode: "production"
+}
