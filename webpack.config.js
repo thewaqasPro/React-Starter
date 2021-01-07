787afa2ea13b32a7939437f34aa4da9a4a712b4c
Muhammad Waqas <waqas.attary@gmail.com>
2021-01-07 20:02:34 +0500

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -7,54 +7,62 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const {WebpackManifestPlugin} = require('webpack-manifest-plugin')
 
-const config = {
-  entry: './app/app.js',
-  output: {
-    filename: 'bundle.[hash].js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  plugins: [new HtmlWebpackPlugin({template: './app/index.html'})],
-  mode: 'development',
-  devtool: 'eval-cheap-source-map',
-  devServer: {
-    port: 8080,
-    contentBase: path.resolve(__dirname, 'dist'),
-    hot: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'] 
-        
-      },
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [ 
-              [
-                '@babel/preset-env', {
-                  'useBuiltIns': 'usage', 
-                  'corejs': 3,
-                  'targets': 'defaults'
+const config = 
+{
+    entry: './app/app.js',
+    output: 
+    {
+        filename: 'bundle.[hash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [ new HtmlWebpackPlugin( {template: './app/index.html'} ) ],
+    mode: 'development',
+    devtool: 'eval-cheap-source-map',
+    devServer: 
+    {
+        port: 8080,
+        contentBase: path.resolve(__dirname, 'dist'),
+        hot: true
+    },
+    module: 
+    {
+        rules: [{
+            test: /\.scss$/,
+            use: ['style-loader', 'css-loader', 'sass-loader'] 
+        },
+        {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: 
+                    [ 
+                        [ 
+                            '@babel/preset-env', 
+                            { 
+                                'useBuiltIns'   : 'usage', 
+                                'corejs'        : 3, 
+                                'targets'       : 'defaults' 
+                            } 
+                        ], 
+                        '@babel/preset-react'
+                    ]
                 }
-              ], 
-              '@babel/preset-react']
-          }
-        }
-      }
-    ]
-  }
+            }
+        }]
+    }
 
 }
 
 if (currentTask == "build")  {
-  config.mode = "production"
-  config.module.rules[0].use[0] = MiniCssExtractPlugin.loader
-  config.plugins.push( new MiniCssExtractPlugin({filename: 'style.[hash].css'}), new CleanWebpackPlugin(), new WebpackManifestPlugin())
+    config.mode = "production"
+    config.module.rules[0].use[0] = MiniCssExtractPlugin.loader
+    config.plugins.push( 
+        new MiniCssExtractPlugin( { filename: 'style.[hash].css' } ), 
+        new CleanWebpackPlugin(), 
+        new WebpackManifestPlugin()
+    )
 }
 
 module.exports = config
